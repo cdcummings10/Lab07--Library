b1ad648b1516031c1e54aa75fa8b76aac999291f
cdcummings10 <ccummings0913@gmail.com>
2019-10-23 13:56:31 -0700

    Finished first few functions.

--- Library/Library/Book.cs
+++ Library/Library/Book.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace Library
 {
-    class Book
+    public enum Genre
     {
+        Fantasy = 1,
+        ScienceFiction,
+        Historical,
+        Biography,
+        YoungAdult,
+        Other
+    }
+    public class Book
+    {
+        public string Title { get; set; }
+        public Author Author { get; set; }
+        public Genre Genre { get; set; }
+
+        public Book(string title, Author author, Genre genre)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+        }
+        
     }
 }
---
+++ Library/Library/Library.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    public class Library<T>: IEnumerable<T>
+    {
+        //Creation of book storage for library
+        private T[] storage = new T[5];
+        private int currentIndex = 0;
+
+        public void Add(T item)
+        {
+            if (storage.Length == currentIndex)
+            {
+                Array.Resize(ref storage, storage.Length * 2);
+            }
+            storage[currentIndex] = item;
+            currentIndex++;
+        }
+
+        public T Remove(int index)
+        {
+            T chosenItem = default;
+            bool picked = false;
+            T[] newStorage = new T[storage.Length - 1];
+            for (int i = 0; i < newStorage.Length; i++)
+            {
+                if (index == i)
+                {
+                    picked = true;
+                    newStorage[i] = storage[i + 1];
+                    chosenItem = storage[i];
+                }
+                else if (picked)
+                {
+                    newStorage[i] = storage[i + 1];
+                }
+                else
+                {
+                    newStorage[i] = storage[i];
+                }
+            }
+            currentIndex = newStorage.Length;
+            storage = newStorage;
+            return chosenItem;
+        }
+
+        //Allows foreach
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < currentIndex; i++)
+            {
+                yield return storage[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
