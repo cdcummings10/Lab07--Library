79cb4d904d6982f419d366cebdb7e6616542035b
lurkor <lurkortv@gmail.com>
2019-10-23 20:44:45 -0700

    Created README for 1.0. Outlined Tests

--- Library/XUnitTestProject1/UnitTest1.cs
+++ Library/XUnitTestProject1/UnitTest1.cs
@@ -6,9 +6,35 @@ namespace XUnitTestProject1
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestIfYouCanAddABookToLibrary()
         {
 
         }
+        [Fact]
+        public void TestIfYouCanCheckOutABook()
+        {
+
+        }
+        [Fact]
+        public void TestIfYouCantRemoveABookThatExists()
+        {
+
+        }
+        [Fact]
+        public void TestIfYouCanCreateANewBook()
+        {
+
+        }
+        [Fact]
+        public void TestIfYouCanCreateANewAuthor()
+        {
+
+        }
+        [Fact]
+        public void TestIfThereIsAnAccurateBookCountInLibrary()
+        {
+
+        }
+        
     }
 }
